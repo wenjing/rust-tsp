efa28095d01b7d50d8be72a6eafb5afba5605abd
Marc Schoolderman <marc@tweedegolf.nl>
2024-02-19 16:43:30 +0100

    add simplified CESR module

---
+++ tsp-cesr/src/decode.rs
@@ -0,0 +1,126 @@
+use super::{bits, extract_triplet, mask};
+
+/// Decode fixed size data with a known identifier
+pub fn decode_fixed_data<'a, const N: usize>(
+    identifier: u32,
+    stream: &mut &'a [u8],
+) -> Option<&'a [u8; N]> {
+    let total_size = (N + 1).next_multiple_of(3);
+    let hdr_bytes = total_size - N;
+
+    let word = match hdr_bytes {
+        1 => bits(identifier, 6) << 18,
+        2 => 52 << 18 | bits(identifier, 6) << 12,
+        3 => 53 << 18 | bits(identifier, 18),
+        _ => unreachable!("unsigned integer arithmetic is broken"),
+    };
+
+    if stream.get(0..hdr_bytes)? == &u32::to_be_bytes(word)[1..=hdr_bytes] {
+        let slice = stream.get(hdr_bytes..total_size)?;
+        *stream = &stream[total_size..];
+
+        Some(slice.try_into().unwrap())
+    } else {
+        None
+    }
+}
+
+/// Decode variable size data with a known identifier
+pub fn decode_variable_data<'a>(identifier: u32, stream: &mut &'a [u8]) -> Option<&'a [u8]> {
+    let input = extract_triplet(stream.get(0..=2)?.try_into().unwrap());
+    let selector = input >> 18;
+
+    let size;
+    let found_id;
+
+    match selector {
+        56..=58 => {
+            found_id = input >> 12 & mask(6);
+            size = input & mask(12);
+        }
+        59..=61 => {
+            found_id = input & mask(18);
+            size = extract_triplet(stream.get(3..6)?.try_into().unwrap());
+        }
+        _ => return None,
+    };
+
+    if found_id == identifier {
+        let offset = (selector - 56) as usize;
+        let data_begin = offset + 3;
+        let data_end = (offset + 1).next_multiple_of(3) + 3 * size as usize;
+        let slice = stream.get(data_begin..data_end)?;
+        *stream = &stream[data_end..];
+
+        Some(slice)
+    } else {
+        None
+    }
+}
+
+/// Decode indexed data with a known identifier
+pub fn decode_indexed_data<'a, const N: usize>(
+    identifier: u32,
+    stream: &mut &'a [u8],
+) -> Option<(u16, &'a [u8; N])> {
+    let total_size = (N + 1).next_multiple_of(3);
+    let hdr_bytes = total_size - N;
+
+    let input = extract_triplet(stream.get(0..=2)?.try_into().unwrap());
+    let word;
+    let index;
+
+    match hdr_bytes {
+        1 => panic!("an indexed type can only have 0 or 2 lead bytes"),
+        2 => {
+            index = input >> 12 & mask(6);
+            word = bits(identifier, 6) << 18 | bits(index, 6) << 12;
+        }
+        3 => {
+            index = input & mask(12);
+            word = 52 << 18 | bits(identifier, 6) << 12 | bits(index, 12);
+        }
+        _ => unreachable!("unsigned integer arithmetic is broken"),
+    };
+
+    if stream.get(0..hdr_bytes)? == &u32::to_be_bytes(word)[1..=hdr_bytes] {
+        let slice = stream.get(hdr_bytes..total_size)?;
+        *stream = &stream[total_size..];
+
+        Some((index as u16, slice.try_into().unwrap()))
+    } else {
+        None
+    }
+}
+
+/// Decode a frame with known identifier and size
+pub fn decode_count(identifier: u16, stream: &mut &[u8]) -> Option<u16> {
+    let word = extract_triplet(stream.get(0..=2)?.try_into().unwrap());
+    let index = word & mask(12);
+
+    let expected = 62 << 18 | bits(identifier, 6) << 12 | bits(index, 12);
+    if word == expected {
+        *stream = &stream[3..];
+
+        Some(index as u16)
+    } else {
+        None
+    }
+}
+
+/// Decode a genus with known identifier and version
+pub fn decode_genus(
+    genus: [u8; 3],
+    (major, minor, patch): (u8, u8, u8),
+    stream: &mut &[u8],
+) -> Option<()> {
+    let version = bits(major, 6) << 12 | bits(minor, 6) << 6 | bits(patch, 6);
+    let word1 = 62 << 18 | 62 << 12 | bits(genus[0], 6) << 6 | bits(genus[1], 6);
+    let word2 = bits(genus[2], 6) << 18 | version;
+
+    (extract_triplet(stream.get(0..3)?.try_into().unwrap()) == word1).then_some(())?;
+    (extract_triplet(stream.get(3..6)?.try_into().unwrap()) == word2).then_some(())?;
+    *stream = &stream[6..];
+
+    Some(())
+}
--- tsp-cesr/src/lib.rs
+++ tsp-cesr/src/lib.rs
@@ -1,14 +1,259 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+mod decode;
+mod encode;
+
+pub use decode::{
+    decode_count, decode_fixed_data, decode_genus, decode_indexed_data, decode_variable_data,
+};
+pub use encode::{
+    encode_count, encode_fixed_data, encode_genus, encode_indexed_data, encode_variable_data,
+};
+
+/// Safely restrict value to a certain number of bits
+fn bits(value: impl Into<u32>, bits: u8) -> u32 {
+    let value = value.into();
+    let mask = (1 << bits as u32) - 1;
+    assert!(value <= mask, "{value} <= {mask}");
+
+    value & mask
+}
+
+/// Produce a bitmask of n bits
+const fn mask(n: u8) -> u32 {
+    (1 << n) - 1
+}
+
+/// Converts a "quadlet" into a u32 (big-endian)
+const fn extract_triplet(quadlet: &[u8; 3]) -> u32 {
+    u32::from_be_bytes([0, quadlet[0], quadlet[1], quadlet[2]])
 }
 
 #[cfg(test)]
-mod tests {
+mod test {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn encode_and_decode() {
+        let mut data = vec![];
+        encode_genus([1, 2, 3], (4, 5, 6), &mut data);
+        encode_fixed_data(2323, b"Hello world!", &mut data); // 0 lead bytes
+        encode_fixed_data(42, b"Tweede golf", &mut data); // 1 lead byte
+        encode_fixed_data(57, b"TrustSpanP", &mut data); // 2 lead byte
+        encode_variable_data(3, b"Where there is power, there is resistance.", &mut data); // 0 lead bytes
+        encode_variable_data(
+            122,
+            b"To pretend, I actually do the thing: I have therefore only pretended to pretend.",
+            &mut data,
+        ); // 1 lead byte
+        encode_variable_data(42,  b"I always speak the truth. Not the whole truth, because there's no way, to say it all.", &mut data); // 2 lead bytes
+        encode_count(7, 2, &mut data);
+        encode_indexed_data(5, 57, b"DON'T PANIC!", &mut data); // 0 lead bytes
+        encode_indexed_data(5, 0, b"SECRET KEY", &mut data); // 2 lead bytes
+
+        let mut input = &data[..];
+        decode_genus([1, 2, 3], (4, 5, 6), &mut input).unwrap();
+        assert_eq!(
+            decode_fixed_data(2323, &mut input).unwrap(),
+            b"Hello world!"
+        );
+        assert_eq!(decode_fixed_data(42, &mut input).unwrap(), b"Tweede golf");
+        assert_eq!(decode_fixed_data(57, &mut input).unwrap(), b"TrustSpanP");
+        assert_eq!(
+            decode_variable_data(3, &mut input).unwrap(),
+            b"Where there is power, there is resistance."
+        );
+        assert_eq!(
+            decode_variable_data(122, &mut input).unwrap(),
+            b"To pretend, I actually do the thing: I have therefore only pretended to pretend."
+        );
+        assert_eq!(decode_variable_data(42, &mut input).unwrap(), b"I always speak the truth. Not the whole truth, because there's no way, to say it all.");
+        assert_eq!(decode_count(7, &mut input).unwrap(), 2);
+        assert_eq!(
+            decode_indexed_data(5, &mut input).unwrap(),
+            (57, b"DON'T PANIC!")
+        );
+        assert_eq!(
+            decode_indexed_data(5, &mut input).unwrap(),
+            (0, b"SECRET KEY")
+        );
+    }
+
+    #[test]
+    fn long_variable_data() {
+        let mut data1 = vec![];
+        let mut data2 = vec![];
+        let mut data3 = vec![];
+        encode_variable_data(0, &[0u8; 4095], &mut data1);
+        encode_variable_data(0, &[0u8; 4096], &mut data2);
+        encode_variable_data(0, &[0u8; 4097], &mut data3);
+        assert!(data1[0] != data2[0]);
+        assert!(data2[0] == data2[0]);
+    }
+
+    #[should_panic]
+    #[test]
+    fn identifier_failure_1() {
+        encode_fixed_data(64, b"Tweede golf", &mut vec![]); // 1 lead byte
+    }
+
+    #[should_panic]
+    #[test]
+    fn identifier_failure_2() {
+        encode_fixed_data(64, b"TrustSpanP", &mut vec![]); // 2 lead bytes
+    }
+
+    #[should_panic]
+    #[test]
+    fn identifier_failure_3() {
+        encode_fixed_data(4096, b"TrustSpanP", &mut vec![]); // 0 lead bytes
+    }
+
+    #[should_panic]
+    #[test]
+    fn identifier_failure_variable() {
+        encode_variable_data(262144, b"", &mut vec![]);
+    }
+
+    #[should_panic]
+    #[test]
+    fn index_failure() {
+        encode_indexed_data(5, 57, b"hello", &mut vec![]); // 1 lead byte
+    }
+
+    #[should_panic]
+    #[test]
+    fn too_long_data_failure() {
+        encode_variable_data(
+            0,
+            &(0..50331646).map(|_| 0).collect::<Vec<u8>>(),
+            &mut vec![],
+        ); // 1 lead byte
+    }
+
+    use base64ct::{Base64Url, Encoding};
+
+    #[test]
+    fn decode_and_encode() {
+        fn fixed_roundtrip<const N: usize>(ident: u32, content: [u8; N], input: &[u8]) {
+            // test that decoding the given output results in the same content
+            let payload = decode_fixed_data(ident, &mut &input[..]).unwrap();
+            assert_eq!(payload, &content);
+
+            // test that encoding the given input leads to the given output
+            let mut output = vec![];
+            encode_fixed_data(ident, &content, &mut output);
+            assert_eq!(input, output);
+        }
+
+        fixed_roundtrip(12, [1, 2], &Base64Url::decode_vec("MAEC").unwrap());
+        fixed_roundtrip(5, [1, 2, 3], &Base64Url::decode_vec("1AAFAQID").unwrap());
+        fixed_roundtrip(7, [1, 2, 3, 4], &Base64Url::decode_vec("0HABAgME").unwrap());
+        fixed_roundtrip(
+            13,
+            [1, 2, 3, 4, 5, 6, 7, 8],
+            &Base64Url::decode_vec("NAECAwQFBgcI").unwrap(),
+        );
+        let mut funky_data = <[u8; 24]>::default();
+        Base64Url::decode("2022-10-25T12c04c30d175309p00c00", &mut funky_data).unwrap();
+        fixed_roundtrip(
+            6,
+            funky_data,
+            &Base64Url::decode_vec("1AAG2022-10-25T12c04c30d175309p00c00").unwrap(),
+        );
+
+        fn variable_roundtrip(ident: u32, content: &[u8], input: &[u8]) {
+            // test that decoding the given output results in the same content
+            let payload = decode_variable_data(ident, &mut &input[..]).unwrap();
+            assert_eq!(payload, content);
+
+            // test that encoding the given input leads to the given output
+            let mut output = vec![];
+            encode_variable_data(ident, &content, &mut output);
+            assert_eq!(input, output);
+        }
+
+        variable_roundtrip(
+            0,
+            &Base64Url::decode_vec("barf").unwrap(),
+            &Base64Url::decode_vec("4AABbarf").unwrap(),
+        );
+
+        variable_roundtrip(
+            0,
+            &Base64Url::decode_vec("AFoo").unwrap()[1..],
+            &Base64Url::decode_vec("5AABAFoo").unwrap(),
+        );
+
+        variable_roundtrip(
+            0,
+            &Base64Url::decode_vec("AAA-field0-field1-field3").unwrap()[2..],
+            &Base64Url::decode_vec("6AAGAAA-field0-field1-field3").unwrap(),
+        );
+
+        variable_roundtrip(1, b"1337", &Base64Url::decode_vec("6BACAAAxMzM3").unwrap());
+
+        variable_roundtrip(
+            1,
+            &[1, 2, 3, 4, 5, 6, 7, 8],
+            &Base64Url::decode_vec("5BADAAECAwQFBgcI").unwrap(),
+        );
+    }
+
+    #[test]
+    fn dont_gen_overlong_encoding() {
+        fn roundtrip(ident: u32, input: &[u8], output: &[u8]) {
+            let payload = decode_variable_data(ident, &mut &input[..]).unwrap();
+            let mut generated = vec![];
+            encode_variable_data(ident, &payload, &mut generated);
+            assert_eq!(generated, output);
+        }
+
+        roundtrip(
+            0,
+            &Base64Url::decode_vec("9AAAAAABAAA-").unwrap(),
+            &Base64Url::decode_vec("6AABAAA-").unwrap(),
+        );
+        roundtrip(
+            1,
+            &Base64Url::decode_vec("8AABAAADAAECAwQFBgcI").unwrap(),
+            &Base64Url::decode_vec("5BADAAECAwQFBgcI").unwrap(),
+        );
+    }
+
+    //NOTE: the official CESR example as several places where padding bits have random values; we have changed:
+    // 1) E_T2_p83_gRSuAYvGhqV3S0JzYEF2dIa-OCPLbIhBO7Y =>
+    //    EPT2_p83_gRSuAYvGhqV3S0JzYEF2dIa-OCPLbIhBO7Y    (padding bits should have a canonical value)
+    // 2) EwmQtlcszNoEIDfqD-Zih3N6o5B3humRKvBBln2juTEM =>
+    //    EAmQtlcszNoEIDfqD-Zih3N6o5B3humRKvBBln2juTEM    (same reason)
+    // 3) AA5267UlFg1jHee4Dauht77SzGl8WUC_0oimYG5If3SdIOSzWM8Qs9SFajAilQcozXJVnbkY5stG_K4NbKdNB4AQ => (1st indexed signature)
+    //    AAB267UlFg1jHee4Dauht77SzGl8WUC_0oimYG5If3SdIOSzWM8Qs9SFajAilQcozXJVnbkY5stG_K4NbKdNB4AQ
+    // 4) ACTD7NDX93ZGTkZBBuSeSGsAQ7u0hngpNTZTK_Um7rUZGnLRNJvo5oOnnC1J2iBQHuxoq8PyjdT3BHS2LiPrs2Cg => (3rd indexed signature)
+    //    ACDD7NDX93ZGTkZBBuSeSGsAQ7u0hngpNTZTK_Um7rUZGnLRNJvo5oOnnC1J2iBQHuxoq8PyjdT3BHS2LiPrs2Cg
+    #[test]
+    fn demo_example() {
+        let base64_data = "\
+-FAB\
+EPT2_p83_gRSuAYvGhqV3S0JzYEF2dIa-OCPLbIhBO7Y\
+-EAB\
+0AAAAAAAAAAAAAAAAAAAAAAB\
+EAmQtlcszNoEIDfqD-Zih3N6o5B3humRKvBBln2juTEM\
+-AAD\
+AAB267UlFg1jHee4Dauht77SzGl8WUC_0oimYG5If3SdIOSzWM8Qs9SFajAilQcozXJVnbkY5stG_K4NbKdNB4AQ\
+ABBgeqntZW3Gu4HL0h3odYz6LaZ_SMfmITL-Btoq_7OZFe3L16jmOe49Ur108wH7mnBaq2E_0U0N0c5vgrJtDpAQ\
+ACDD7NDX93ZGTkZBBuSeSGsAQ7u0hngpNTZTK_Um7rUZGnLRNJvo5oOnnC1J2iBQHuxoq8PyjdT3BHS2LiPrs2Cg\
+";
+
+        let data = Base64Url::decode_vec(base64_data).unwrap();
+
+        let slice = &mut &data[..];
+
+        assert_eq!(decode_count(5, slice).unwrap(), 1);
+        decode_fixed_data::<32>(4, slice).unwrap();
+        assert_eq!(decode_count(4, slice).unwrap(), 1);
+        decode_fixed_data::<16>(0, slice).unwrap();
+        decode_fixed_data::<32>(4, slice).unwrap();
+        assert_eq!(decode_count(0, slice).unwrap(), 3);
+        assert_eq!(decode_indexed_data::<64>(0, slice).unwrap().0, 0);
+        assert_eq!(decode_indexed_data::<64>(0, slice).unwrap().0, 1);
+        assert_eq!(decode_indexed_data::<64>(0, slice).unwrap().0, 2);
     }
 }
