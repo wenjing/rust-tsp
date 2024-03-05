bfd407281915e214238a2f77e58cd81d324ad98c
Marlon Baeten <marlon@tweedegolf.com>
2024-03-05 15:37:07 +0100

    Added rustdoc to tsp crate

--- tsp/src/lib.rs
+++ tsp/src/lib.rs
@@ -12,15 +12,15 @@ pub use vid_database::VidDatabase;
 ///
 /// * `vid` - A VID, for example `did:web:did.tsp-test.org:user:bob`
 ///
-/// # Examples
+/// # Example
 ///
 /// ```
 /// #[tokio::main]
 /// async fn main() {
 ///     use tsp_definitions::ResolvedVid;
-/// 
+///
 ///     let relation = tsp::resolve_vid("did:web:did.tsp-test.org:user:bob").await.unwrap();
-/// 
+///
 ///     assert_eq!(relation.endpoint().as_str(), "tcp://127.0.0.1:1337");
 /// }
 /// ```
@@ -28,7 +28,29 @@ pub async fn resolve_vid(vid: &str) -> Result<Vid, Error> {
     tsp_vid::resolve::resolve_vid(vid).await
 }
 
-// send data to a resolved VID using the TSP
+/// Send data to a resolved VID using the TSP
+/// Encodes, encrypts, signs and sends a TSP message
+///
+/// # Arguments
+///
+/// * `sender`               - A sender identity implementing the trait `Sender`
+/// * `receiver`             - A receiver identity implementing the trait `ResolvedVid`
+/// * `nonconfidential_data` - Optional extra non-confidential data
+/// * `payload`              - The raw message payload as byte slice
+///
+/// # Example
+///
+/// ```
+/// #[tokio::main]
+/// async fn main() {
+///     use tsp_vid::VidController;
+///
+///     let sender = VidController::from_file("../examples/test/alice.identity").await.unwrap();
+///     let receiver = tsp::resolve_vid("did:web:did.tsp-test.org:user:bob").await.unwrap();
+///
+///     let result = tsp::send(&sender, &receiver, None, b"hello world").await;
+/// }
+/// ```
 pub async fn send(
     sender: &impl Sender,
     receiver: &impl ResolvedVid,
@@ -41,7 +63,31 @@ pub async fn send(
     Ok(())
 }
 
-// listen for incopming TSP messages, given the transport provided by the receiver VID
+/// Listen for incopming TSP messages, given the transport provided by the receiver VID
+///
+/// # Arguments
+///
+/// * `receiver`  - A receiver identity implementing the trait `Receiver`
+/// * `listening` - Optional signal receiver, notified when the receiver is ready to accept incoming messages
+///
+/// # Example
+///
+/// ```
+/// #[tokio::main]
+/// async fn main() {
+///     use tsp_vid::VidController;
+///     use futures::StreamExt;
+///
+///     let receiver = VidController::from_file("../examples/test/bob.identity").await.unwrap();
+///
+///     let messages = tsp::receive(&receiver, None).unwrap();
+///     tokio::pin!(messages);
+///
+///     while let Some(Ok(msg)) = messages.next().await {
+///         println!("Received {:?}", msg.payload);
+///     }
+/// }
+/// ```
 pub fn receive(
     receiver: &impl Receiver,
     listening: Option<tokio::sync::oneshot::Sender<()>>,
